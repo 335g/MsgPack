5a2dfd01a84e66bebbd42abe024dc228c7d01ab5
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2017-09-18 16:12:16 +0900

    Forgot array

---
+++ Tests/MsgPackTests/EncoderTests.swift
@@ -0,0 +1,13 @@
+//
+//  EncoderTests.swift
+//  MsgPackTests
+//
+//  Created by Yoshiki Kudo on 2017/09/18.
+//
+
+import XCTest
+@testable import MsgPack
+
+class EncoderTests: XCTestCase {
+    
+}
